9fac712304ad0c01756b68e33599077ead814570
juliantjg <juliantj88@gmail.com>
2022-02-18 15:00:43 +1100

    human time difference for event creation time

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,11 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import *
+from base.views.baseViews import getHumanTimeDifferenceToNow
 
 class EventSerializer(serializers.ModelSerializer):
+    humanTimeDiffCreatedAt = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Event
         fields = '__all__'
+
+    def get_humanTimeDiffCreatedAt(self, obj):
+        return getHumanTimeDifferenceToNow(obj.createdAt)
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
