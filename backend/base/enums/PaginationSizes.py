24d4aaba09dbdbec09921b0b46f33da1f19f1c77
juliantjg <juliantj88@gmail.com>
2022-07-22 10:57:01 +1000

    get all users paginated (#28)

---
+++ backend/base/enums/PaginationSizes.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class get(Enum):
+    XS = 5
+    S  = 10
+    M  = 15
+    L  = 20
+    XL = 25
--- backend/base/modules/user/api/actions/GetAllUserAction.py
+++ backend/base/modules/user/api/actions/GetAllUserAction.py
@@ -1,9 +1,9 @@
 from base.models import User
 from base.serializers import UserSerializer
-from base.views.baseViews import response, error
+from base.views.baseViews import response, error, paginate
+from base.enums import PaginationSizes
 
 def get(request):
-    users = User.objects.all()
-    serializer = UserSerializer(users, many=True)
-
-    return response(serializer.data)
+    users = User.objects.all().order_by('id')
+    
+    return paginate(request, users, UserSerializer, PaginationSizes.get.S.value)
