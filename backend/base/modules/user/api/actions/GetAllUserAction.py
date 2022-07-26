d333ad0f1462c4facf15a12a8cc8ee93e7cb81a9
juliantjg <juliantj88@gmail.com>
2022-07-26 18:25:22 +1000

    sort/filter early stage (#28)

--- backend/base/modules/user/api/actions/GetAllUserAction.py
+++ backend/base/modules/user/api/actions/GetAllUserAction.py
@@ -3,7 +3,23 @@ from base.serializers import UserSerializer
 from base.views.baseViews import response, error, paginate
 from base.enums import PaginationSizes
 
+def filterOrSort(request):
+    # the only way to do filter+sort is to .filter().sort()
+    # sort wil always come after User.objects, User.objects.sort()
+    filtered = 0
+    users = User.objects.none()
+    if request.GET.get('username') != None:
+        username = request.GET.get('username')
+        users = User.objects.filter(username__contains=username)
+        filtered = 1
+
+    sort = request.GET.get('sort')
+    if filtered == 0:
+        users = User.objects.order_by('')
+
+    return users
+
 def get(request):
-    users = User.objects.all().order_by('id')
+    users = filterOrSort(request)
     
     return paginate(request, users, UserSerializer, PaginationSizes.get.S.value)
