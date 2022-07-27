ab380b0a16ce670cac812c80ae084ceb4738b130
juliantjg <juliantj88@gmail.com>
2022-07-28 00:56:23 +1000

    get participated not done fiilter (#28)

--- backend/base/modules/event/api/actions/GetParticipatedAndOwnedEventsAction.py
+++ backend/base/modules/event/api/actions/GetParticipatedAndOwnedEventsAction.py
@@ -3,6 +3,36 @@ from base.serializers import EventSerializer
 from base.views.baseViews import response, error
 from base.enums import EventMemberStatus
 
+def validateFilter(filterField):
+    allowedFilters = ['title', 'userName', 'maxMember']
+    if filterField not in allowedFilters:
+        return False
+    return True
+
+def filterOrSort(request):
+    # How to filter: filter=userName-test
+
+    filterField = ''
+    filterValue = ''
+    if request.GET.get('filter') != None:
+        filterString = request.GET.get('filter', '')
+        filterField  = filterString.split('-')[0]
+        filterValue  = filterString.split('-')[1]
+
+        if not validateFilter(filterField):
+            return 'Targeted filter field not found'
+
+    if filterField != '' and sortField != '':
+        filterFieldContains = filterField + '__icontains'
+        return Event.objects.filter(**{filterFieldContains: filterValue}).order_by(sortField)
+    elif filterField != '':
+        filterFieldContains = filterField + '__icontains'
+        return Event.objects.filter(**{filterFieldContains: filterValue})
+    elif sortField != '':
+        return Event.objects.all().order_by(sortField)
+    else:
+        return Event.objects.all()
+
 def get(request):
     user = request.user
 
