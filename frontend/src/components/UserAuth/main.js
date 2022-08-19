4f2a50888492c6914ade50927cfb48c20b38969b
Manan Lodhia <mananlodhia@hotmail.com>
2022-08-20 00:41:37 +1000

    added reset pw component

--- frontend/src/components/UserAuth/main.js
+++ frontend/src/components/UserAuth/main.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Row, Col, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Footer from "../SiteElements/footer";
 import Login from "./log-in";
+import ResetPassword from "./reset-pw";
 import SignUp from "./sign-up";
 
 function Main() {
@@ -11,17 +12,23 @@ function Main() {
         <div className="auth-content">
           <div>
             <h1 className="titleText text-center">eventmatcher</h1>
-            {window.location.pathname === "/signup" ? <SignUp /> : <Login />}
+            {window.location.pathname === "/signup" ? (
+              <SignUp />
+            ) : window.location.pathname === "/reset-password" ? (
+              <ResetPassword />
+            ) : (
+              <Login />
+            )}
           </div>
         </div>
         <div className="auth-footer">
           {window.location.pathname === "/signup" ? (
             <h6>
-              Already have an account? <a href="/">Log in</a>
+              Already have an account? <Link to="/">Log in</Link>
             </h6>
           ) : (
             <h6>
-              Don't have an account? <a href="/signup">Sign Up</a>
+              Don't have an account? <Link to="/signup">Sign Up</Link>
             </h6>
           )}
         </div>
