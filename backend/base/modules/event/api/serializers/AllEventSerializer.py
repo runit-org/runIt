35e5d2115972d9173c7eb845474048cd735b775e
juliantjg <juliantj88@gmail.com>
2022-08-20 20:45:24 +1000

    fix error

--- backend/base/modules/event/api/serializers/AllEventSerializer.py
+++ backend/base/modules/event/api/serializers/AllEventSerializer.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 from base.models import *
 from base.traits import GetHumanTimeDifferenceToNow, CheckUserMemberEvent
 
-class EventSerializer(serializers.ModelSerializer):
+class AllEventSerializer(serializers.ModelSerializer):
     humanTimeDiffCreatedAt = serializers.SerializerMethodField(read_only=True)
+    joinedStatus = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Event
@@ -12,3 +13,7 @@ class EventSerializer(serializers.ModelSerializer):
 
     def get_humanTimeDiffCreatedAt(self, obj):
         return GetHumanTimeDifferenceToNow.get(obj.createdAt)
+    
+    def get_joinedStatus(self, obj):
+        authUserId = self.context.get('userId')
+        return CheckUserMemberEvent.get(obj.id, authUserId)
