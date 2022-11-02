2bcc306b4c9621264605025d26a046adaf6027ca
Manan Lodhia <mananlodhia@hotmail.com>
2022-11-02 17:09:16 +1100

    fix/mentions function now allows taggin multiple strings

--- frontend/src/components/Utilities/mention.js
+++ frontend/src/components/Utilities/mention.js
@@ -1,8 +1,6 @@
-export const Mention = (item) => {
- 
-    var tag = item.match(/(?:^|[ ])@([a-zA-Z]+[0-9])/gm)
-        
-    return (
-        item.replace(tag, '<a href="#">'+tag+ '</a>')
-    )
+export const Mention = (text) => {
+  const regex = /\B@[a-zA-Z0-9_-]+/gm;
+  return (text = text.replace(regex, (match) => {
+    return `<a href="/profile?user=${match.substring(1)}">${match}</a>`;
+  }));
 };
