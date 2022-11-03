cfc79ba02c56b9ca539470e8793376be6c7d285e
juliantjg <juliantj88@gmail.com>
2022-11-03 11:56:25 +1100

    tag user on create event will notify

--- backend/base/modules/event/api/actions/CreateEventAction.py
+++ backend/base/modules/event/api/actions/CreateEventAction.py
@@ -1,7 +1,26 @@
-from base.models import Event
+from base.models import Event, User, EventMember
 from base.serializers import EventSerializer
+from base.enums import EventMemberStatus
+from base.traits import NotifyUser
 from base.views.baseViews import response, error
 
+def mention(event, content, user):
+    characters = content.split(' ')
+    for i in characters:
+        targetUsername = ''
+        if i[0] == '@':
+            targetUsername = i[1:]
+
+        # Check if the mentioned user exist in the database first
+        if len(User.objects.filter(username = targetUsername)) > 0:
+            targetUser = User.objects.get(username = targetUsername)
+
+            # Tagging yourself wouldn't notify
+            if targetUser != user:
+                notificationMessage = 'User <b>' + user.username + '</b> mentioned you on event ' + '<b>' + event.title + '</b>. Message: <i>' + content + '</i>'
+                NotifyUser.notify(targetUser.id, notificationMessage)
+
+               
 def create(request):
     data = request.data
     user = request.user
@@ -19,5 +38,7 @@ def create(request):
         minute      = data['minute'],
     )
     serializer = EventSerializer(event, many=False)
+
+    mention(event, event.details, user)
 
     return response('Event created', serializer.data)
