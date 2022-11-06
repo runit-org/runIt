0528b28be43b18ad79cfc975aebc9fe43ff487a1
juliantjg <juliantj88@gmail.com>
2022-11-07 00:00:20 +1100

    added user on get one event

--- backend/base/modules/event/api/serializers/EventSerializer.py
+++ backend/base/modules/event/api/serializers/EventSerializer.py
@@ -12,7 +12,7 @@ class EventSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Event
-        fields = ['id', 'userName', 'title', 'maxMember', 'details', 'createdAt', 'humanTimeDiffCreatedAt', 'eventDateString', 'eventDate']
+        fields = ['id', 'userName', 'title', 'maxMember', 'details', 'createdAt', 'humanTimeDiffCreatedAt', 'eventDateString', 'eventDate', 'user']
 
     def get_humanTimeDiffCreatedAt(self, obj):
         return GetHumanTimeDifferenceToNow.get(obj.createdAt)
