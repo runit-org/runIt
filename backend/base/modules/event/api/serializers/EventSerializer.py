838b024ecc8d020e4fcd54526aa8a215763aa0fb
juliantjg <juliantj88@gmail.com>
2022-11-16 00:18:03 +1100

    added join status on view one event

--- backend/base/modules/event/api/serializers/EventSerializer.py
+++ backend/base/modules/event/api/serializers/EventSerializer.py
@@ -9,10 +9,11 @@ class EventSerializer(serializers.ModelSerializer):
     humanTimeDiffCreatedAt = serializers.SerializerMethodField(read_only=True)
     eventDateString = serializers.SerializerMethodField(read_only=True)
     eventDate = serializers.SerializerMethodField(read_only=True)
+    joinedStatus = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Event
-        fields = ['id', 'userName', 'title', 'maxMember', 'details', 'createdAt', 'humanTimeDiffCreatedAt', 'eventDateString', 'eventDate', 'user']
+        fields = ['id', 'userName', 'title', 'maxMember', 'details', 'createdAt', 'humanTimeDiffCreatedAt', 'eventDateString', 'eventDate', 'user', 'joinedStatus']
 
     def get_humanTimeDiffCreatedAt(self, obj):
         return GetHumanTimeDifferenceToNow.get(obj.createdAt)
@@ -22,3 +23,10 @@ class EventSerializer(serializers.ModelSerializer):
     
     def get_eventDate(self, obj):
         return datetime(obj.year, obj.month, obj.day, obj.hour, obj.minute)
+
+    def get_joinedStatus(self, obj):
+        if self.context.get('userId') != None:
+            authUserId = self.context.get('userId')
+            return CheckUserMemberEvent.get(obj.id, authUserId)
+        else:
+            return None
