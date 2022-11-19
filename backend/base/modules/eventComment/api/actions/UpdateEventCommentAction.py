e0861bf97667e79aef89f234a69c9e60a16fe0be
juliantjg <juliantj88@gmail.com>
2022-11-19 21:00:39 +1100

    mention user on update comment

--- backend/base/modules/eventComment/api/actions/UpdateEventCommentAction.py
+++ backend/base/modules/eventComment/api/actions/UpdateEventCommentAction.py
@@ -2,6 +2,7 @@ from base.models import Event, User, EventComment, EventMember
 from base.serializers import EventCommentSerializer
 from base.views.baseViews import response, error, paginate
 from base.enums import EventMemberStatus
+from base.traits import NotifyUser
 
 def checkCommentId(id):
     checkCommentExist = EventComment.objects.filter(id = id)
@@ -10,6 +11,27 @@ def checkCommentId(id):
         return True
     else:
         return False
+
+def mention(event, content, user):
+    characters = content.split(' ')
+    for i in characters:
+        targetUsername = ''
+        if i[0] == '@':
+            targetUsername = i[1:]
+
+        # Check if the mentioned user exist in the database first
+        if len(User.objects.filter(username = targetUsername)) > 0:
+            targetUser = User.objects.get(username = targetUsername)
+
+            checkIfTargetUserIsMember = EventMember.objects.filter(eventId = event.id, userId = targetUser.id, status = EventMemberStatus.get.ACCEPTED.value)
+
+            # Tagging yourself wouldn't notify
+            if targetUser != user:
+
+                # Then check if the mentioned user is an ACCEPTED member of the current event OR is the event's creator
+                if len(checkIfTargetUserIsMember) > 0 or targetUser == event.user:
+                    notificationMessage = 'User <b>' + user.username + '</b> mentioned you in a comment on event ' + '<b>' + event.title + '</b>. Message: <i>' + content + '</i>'
+                    NotifyUser.notify(targetUser.id, notificationMessage)
 
 def checkCommentCreator(commentId, userId):
     comment = EventComment.objects.get(id=commentId)
@@ -33,6 +55,9 @@ def update(request, commentId):
     comment.save()
 
     serializer = EventCommentSerializer(comment, many=False)
+
+    event = comment.event
+    mention(event, data['content'], user)
 
     return response('Comment updated', serializer.data)
     
