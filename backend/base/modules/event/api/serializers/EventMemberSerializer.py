d89dfcc394021de4394910cb9038a160d04b7795
juliantjg <juliantj88@gmail.com>
2022-11-20 22:17:00 +1100

    added gravatar on event members

--- backend/base/modules/event/api/serializers/EventMemberSerializer.py
+++ backend/base/modules/event/api/serializers/EventMemberSerializer.py
@@ -2,11 +2,13 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from base.models import *
 from base.enums import EventMemberStatus
+from base.traits import CreateGravatarProfile
 
 class EventMemberSerializer(serializers.ModelSerializer):
     username = serializers.SerializerMethodField(read_only=True)
     status   = serializers.SerializerMethodField(read_only=True)
     email    = serializers.SerializerMethodField(read_only=True)
+    gravatarImage = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = EventMember
@@ -22,3 +24,7 @@ class EventMemberSerializer(serializers.ModelSerializer):
     def get_email(self, obj):
         user = User.objects.get(id = obj.userId).email
         return user
+
+    def get_gravatarImage(self, obj):
+        user = User.objects.get(id = obj.userId)
+        return CreateGravatarProfile.create(user.email)
