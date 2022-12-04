ed0c9b2253f9c1196fafb3cf120303829246d41f
juliantjg <juliantj88@gmail.com>
2022-12-05 00:01:46 +1100

    login test (not done)

--- backend/base/tests/AuthTest.py
+++ backend/base/tests/AuthTest.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from base.models import User
 from django.contrib.auth.hashers import make_password
+from rest_framework import status
+from rest_framework.test import APIRequestFactory
 
 class AuthTestClass(TestCase):
     def setUp(self):
@@ -10,5 +12,14 @@ class AuthTestClass(TestCase):
             password = make_password('password')
         )
 
-    def test_first_test(self):
-        self.assertEqual(1, 1)
+    def test_login_success(self):
+        url = '/auth/login/'
+        data = {
+            'username': 'test',
+            'email': "test@email.com",
+            'password': 'password'
+        }
+        response = self.client.post(url, data, format='json')
+        print(response)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
