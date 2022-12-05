eaf5de7e1f53ab3a13e3532cee2ab1530b735596
juliantjg <juliantj88@gmail.com>
2022-12-05 11:20:46 +1100

    added login and register tests

--- backend/base/tests/AuthTest.py
+++ backend/base/tests/AuthTest.py
@@ -5,21 +5,104 @@ from rest_framework import status
 from rest_framework.test import APIRequestFactory
 
 class AuthTestClass(TestCase):
+    newUser = None
+    baseUrl = '/api/auth/'
+
     def setUp(self):
+        self.newUser = {
+            "name": "test user",
+            "username": "test",
+            "email": "test@email.com",
+            "password": "password"
+        }
+
+    def createNewUser(self):
         User.objects.create(
-            username = 'test',
-            email    = 'test@email.com',
-            password = make_password('password')
+            username = self.newUser['username'],
+            email    = self.newUser['email'],
+            password = make_password(self.newUser['password'])
         )
 
     def test_login_success(self):
-        url = '/auth/login/'
+        url = self.baseUrl + 'login/'
+
+        self.createNewUser()
         data = {
-            'username': 'test',
-            'email': "test@email.com",
-            'password': 'password'
+            'username': self.newUser['username'],
+            'password': self.newUser['password']
         }
         response = self.client.post(url, data, format='json')
-        print(response)
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login_empty_fields_fails(self):
+        url = self.baseUrl + 'login/'
+
+        self.createNewUser()
+        data = {
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertFalse(response.status_code == status.HTTP_200_OK)
+
+    def test_login_invalid_credentials_fails(self):
+        url = self.baseUrl + 'login/'
+
+        self.createNewUser()
+        data = {
+            'username': self.newUser['username'] + 'zzz',
+            'password': self.newUser['password'] + 'zzz'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_register_success(self):
+        url = self.baseUrl + 'register/'
+
+        data = {
+            'name': self.newUser['name'],
+            'username': self.newUser['username'],
+            'email': self.newUser['email'],
+            'password': self.newUser['password'],
+            'c_password': self.newUser['password']
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(User.objects.get(username=self.newUser['username']).username == self.newUser['username'])
+
+    def test_register_empty_fields_fails(self):
+        url = self.baseUrl + 'register/'
+
+        data = {
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertFalse(response.status_code == status.HTTP_200_OK)
+
+    def test_register_user_confirm_password_not_match_fails(self):
+        url = self.baseUrl + 'register/'
+
+        data = {
+            'name': self.newUser['name'],
+            'username': self.newUser['username'],
+            'email': self.newUser['email'],
+            'password': self.newUser['password'],
+            'c_password': self.newUser['password'] + 'zzz'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def test_register_user_username_and_email_taken_fails(self):
+        url = self.baseUrl + 'register/'
+
+        self.createNewUser()
+        data = {
+            'name': self.newUser['name'],
+            'username': self.newUser['username'],
+            'email': self.newUser['email'],
+            'password': self.newUser['password'],
+            'c_password': self.newUser['password']
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+
+
