6d0f1038300943f667e77499dda473cbc39b50f3
juliantjg <juliantj88@gmail.com>
2022-12-22 12:11:44 +1100

    more event tests excluding member tests

--- backend/base/tests/EventTest.py
+++ backend/base/tests/EventTest.py
@@ -1,12 +1,12 @@
 from django.test import TestCase
 from django.test import Client
-from base.models import User, UserExtend, UserVote, Event
+from base.models import User, UserExtend, UserVote, Event, EventMember
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 from rest_framework.test import force_authenticate
-from base.enums import UserVoteStatus
+from base.enums import UserVoteStatus, EventStatus, EventMemberStatus
 import random
 import string
 import datetime
@@ -53,17 +53,16 @@ class EventTestClass(TestCase):
         )
 
     def generateNewEventData(self):
-        now = datetime.date.today() + datetime.timedelta(days=1)
+        tomorrow = datetime.date.today() + datetime.timedelta(days=1)
         newEvent = {
             "title"       : self.generateRandomString(5),
             "maxMember"   : 3,
-            "userName"    : self.generateRandomString(5),
             "details"     : self.generateRandomString(5),
-            "year"        : now.year,
-            "month"       : now.month,
-            "day"         : now.day,
-            "hour"        : 20,
-            "minute"      : 20,
+            "year"        : tomorrow.year,
+            "month"       : tomorrow.month,
+            "day"         : tomorrow.day,
+            "hour"        : 23,
+            "minute"      : 59,
         }
         return newEvent
     
@@ -161,5 +160,281 @@ class EventTestClass(TestCase):
         eventData['year'] = 2000
         response = c.post(url, eventData, format='json')
         self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def test_view_event_success(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'view/' + str(eventObject.id) + '/'
+
+        # Authenticate user-------------------------------------------
+        self.createNewUser()
+        user = User.objects.get(username=self.newUser['username'])
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        response = c.get(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(eventObject.userName, response.json()['data']['userName'])
+        self.assertEqual(eventObject.id, response.json()['data']['id'])
+
+    def test_view_event_id_not_found_fails(self):
+        url = self.baseUrl + 'view/1/'
+
+        # Authenticate user-------------------------------------------
+        self.createNewUser()
+        user = User.objects.get(username=self.newUser['username'])
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        response = c.get(url, {}, format='json')
+        self.assertFalse(response.status_code == status.HTTP_200_OK)
+
+    def test_update_event_success(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'update/' + str(eventObject.id) + '/'
+
+        # The authenticated user must be the event creator
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=eventObject.userName)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        data = self.generateNewEventData()
+        response = c.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(data['title'], Event.objects.get(id=eventObject.id).title)
+
+    def test_update_event_not_event_owner_fails(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'update/' + str(eventObject.id) + '/'
+
+        # Test updating as not event owner fails
+        # Authenticate user-------------------------------------------
+        self.createNewUser()
+        user = User.objects.get(username=self.newUser['username'])
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        data = self.generateNewEventData()
+        response = c.put(url, data, format='json')
+        self.assertFalse(response.status_code == status.HTTP_200_OK)
+
+    def test_update_event_with_empty_array_data_fails(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'update/' + str(eventObject.id) + '/'
+
+        # The authenticated user must be the event creator
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=eventObject.userName)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        data = {}
+        response = c.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def test_update_event_less_than_two_max_members_fails(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'update/' + str(eventObject.id) + '/'
+
+        # The authenticated user must be the event creator
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=eventObject.userName)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        data = self.generateNewEventData()
+        data['maxMember'] = 1
+        response = c.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def test_update_event_status_success(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'updateStatus/' + str(eventObject.id) + '/'
+        eventStatus = EventStatus.get.CANCELLED.value
+
+        # The authenticated user must be the event creator
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=eventObject.userName)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        data = {
+            "status" : eventStatus
+        }
+        response = c.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(eventStatus, Event.objects.get(id=eventObject.id).status)
+
+    def test_update_event_status_not_event_owner_fails(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'updateStatus/' + str(eventObject.id) + '/'
+        eventStatus = EventStatus.get.CANCELLED.value
+
+        # Test updating as not event owner fails
+        # Authenticate user-------------------------------------------
+        self.createNewUser()
+        user = User.objects.get(username=self.newUser['username'])
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        data = {
+            "status" : eventStatus
+        }
+        response = c.patch(url, data, format='json')
+        self.assertFalse(response.status_code == status.HTTP_200_OK)
+
+    def test_update_event_status_with_empty_data_array_fails(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'updateStatus/' + str(eventObject.id) + '/'
+
+        # The authenticated user must be the event creator
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=eventObject.userName)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        data = {}
+        response = c.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def test_update_event_status_invalid_status_fails(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'updateStatus/' + str(eventObject.id) + '/'
+        eventStatus = EventStatus.get.PENDING.value
+
+        # The authenticated user must be the event creator
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=eventObject.userName)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        data = {
+            "status" : eventStatus
+        }
+        response = c.patch(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def test_delete_event_success(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'delete/' + str(eventObject.id) + '/'
+
+        # The authenticated user must be the event creator
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=eventObject.userName)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        response = c.delete(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(Event.objects.filter(id=eventObject.id)) < 1)
+
+    def test_delete_event_id_not_found_fails(self):
+        url = self.baseUrl + 'delete/1/'
+
+        # The authenticated user must be the event creator
+        # Authenticate user-------------------------------------------
+        self.createNewUser()
+        user = User.objects.get(username=self.newUser['username'])
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        response = c.delete(url, {}, format='json')
+        self.assertFalse(response.status_code == status.HTTP_200_OK)
+
+    def test_delete_event_as_not_event_owner_fails(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'delete/' + str(eventObject.id) + '/'
+
+        # Test deleting event as not event owner fails
+        # Authenticate user-------------------------------------------
+        self.createNewUser()
+        user = User.objects.get(username=self.newUser['username'])
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        response = c.delete(url, {}, format='json')
+        self.assertFalse(response.status_code == status.HTTP_200_OK)
+
+    def test_get_user_owned_events_success(self):
+        # Create 2 event objects under the same user
+        eventObject = self.generateNewEventObject()
+        eventObject2 = self.generateNewEventObject()
+        eventObject2.user = eventObject.user
+        eventObject2.save()
+        url = self.baseUrl + 'owned/'
+
+        # The authenticated user must be the event creator
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=eventObject.userName)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        response = c.get(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check if the user gets two events from the get owned events api
+        self.assertEqual(2, response.json()['count'])
+
+    def test_get_user_affiliated_events_success(self):
+        # Create 2 event objects under the same user
+        eventObject = self.generateNewEventObject()
+        eventObject2 = self.generateNewEventObject()
+        eventObject2.user = eventObject.user
+        eventObject2.save()
+        url = self.baseUrl + 'affiliated/'
+
+        # The authenticated user must be the event creator
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=eventObject.userName)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        # Create an event member object with this user being assigned to a different event
+        eventObjectAnother = self.generateNewEventObject()
+        EventMember.objects.create(
+            userId = user.id,
+            user = user,
+            eventId = eventObjectAnother.id,
+            event = eventObjectAnother,
+            status = EventMemberStatus.get.ACCEPTED.value,
+        )
+
+        response = c.get(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check if the user gets three events (2 owned + 1 joined)
+        self.assertEqual(3, response.json()['count'])
+
+    def test_event_owner_make_announcement_success(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'announce/' + str(eventObject.id) + '/'
+
+        # The authenticated user must be the event creator
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=eventObject.userName)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        data = {
+            "content" : "Hello World"
+        }
+        response = c.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    
 
 
