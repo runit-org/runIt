bfb12ac095049fd1fbdd8c6a0846e9facfe28d5e
juliantjg <juliantj88@gmail.com>
2022-12-23 16:59:11 +1100

    show and create event comment tests

--- backend/base/tests/EventTest.py
+++ backend/base/tests/EventTest.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.test import Client
-from base.models import User, UserExtend, UserVote, Event, EventMember
+from base.models import User, UserExtend, UserVote, Event, EventMember, EventComment
 from django.contrib.auth.hashers import make_password
 from rest_framework import status
 from rest_framework.authtoken.models import Token
@@ -753,3 +753,253 @@ class EventTestClass(TestCase):
         }
         response = c.post(url, data, format='json')
         self.assertFalse(response.status_code == status.HTTP_200_OK)
+
+    def test_create_event_comment_as_event_owner_success(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'comment/create/' + str(eventObject.id) + '/'
+
+        # Authenticated user is event owner
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=eventObject.userName)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+        
+        data = {
+            "content" : "Hello World"
+        }
+        response = c.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(EventComment.objects.get(user=user, event=eventObject).content == data['content'])
+
+    def test_create_event_comment_as_accepted_event_member_success(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'comment/create/' + str(eventObject.id) + '/'
+
+        # Authenticated user is an accepted event member
+        # Authenticate user-------------------------------------------
+        self.createNewUser()
+        user = User.objects.get(username=self.newUser['username'])
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        EventMember.objects.create(
+            user = user,
+            userId = user.id,
+            event = eventObject,
+            eventId = eventObject.id,
+            status = EventMemberStatus.get.ACCEPTED.value
+        )
+
+        data = {
+            "content" : "Hello World"
+        }
+        response = c.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(EventComment.objects.get(user=user, event=eventObject).content == data['content'])
+
+    def test_create_event_comment_with_empty_data_array_fails(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'comment/create/' + str(eventObject.id) + '/'
+
+        # Authenticated user is event owner
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=eventObject.userName)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+        
+        data = {}
+        response = c.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def test_create_event_comment_as_non_member_fails(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'comment/create/' + str(eventObject.id) + '/'
+
+        # Authenticated user is an accepted event member
+        # Authenticate user-------------------------------------------
+        self.createNewUser()
+        user = User.objects.get(username=self.newUser['username'])
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        data = {
+            "content" : "Hello World"
+        }
+        response = c.post(url, data, format='json')
+        self.assertFalse(response.status_code == status.HTTP_200_OK)
+
+    def test_create_event_comment_as_non_accepted_member_fails(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'comment/create/' + str(eventObject.id) + '/'
+
+        # Authenticated user is an accepted event member
+        # Authenticate user-------------------------------------------
+        self.createNewUser()
+        user = User.objects.get(username=self.newUser['username'])
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        EventMember.objects.create(
+            user = user,
+            userId = user.id,
+            event = eventObject,
+            eventId = eventObject.id,
+            status = EventMemberStatus.get.PENDING.value
+        )
+
+        data = {
+            "content" : "Hello World"
+        }
+        response = c.post(url, data, format='json')
+        self.assertFalse(response.status_code == status.HTTP_200_OK)
+
+    def test_create_event_comment_on_a_non_active_event_fails(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'comment/create/' + str(eventObject.id) + '/'
+
+        # Authenticated user is an accepted event member
+        # Authenticate user-------------------------------------------
+        self.createNewUser()
+        user = User.objects.get(username=self.newUser['username'])
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        EventMember.objects.create(
+            user = user,
+            userId = user.id,
+            event = eventObject,
+            eventId = eventObject.id,
+            status = EventMemberStatus.get.PENDING.value
+        )
+
+        eventObject.status = EventStatus.get.FINISHED.value
+        eventObject.save()
+
+        data = {
+            "content" : "Hello World"
+        }
+        response = c.post(url, data, format='json')
+        self.assertFalse(response.status_code == status.HTTP_200_OK)
+
+    def test_show_event_comments_as_event_owner_success(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'comment/show/' + str(eventObject.id) + '/'
+
+        # Authenticated user is event owner
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=eventObject.userName)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        member1 = self.generateNewUserObject()
+        EventComment.objects.create(
+            user = member1,
+            event = eventObject,
+            content = "Hello World",
+            createdAt = datetime.datetime.now()
+        )
+        
+        response = c.get(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.json()['count'] == 1)
+
+    def test_show_event_comment_as_an_accepted_member_success(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'comment/show/' + str(eventObject.id) + '/'
+
+        # Authenticated user is event owner
+        # Authenticate user-------------------------------------------
+        self.createNewUser()
+        user = User.objects.get(username=self.newUser['username'])
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        EventMember.objects.create(
+            eventId = eventObject.id,
+            event = eventObject,
+            userId = user.id,
+            user = user,
+            status = EventMemberStatus.get.ACCEPTED.value
+        )
+
+        member1 = self.generateNewUserObject()
+        EventComment.objects.create(
+            user = member1,
+            event = eventObject,
+            content = "Hello World",
+            createdAt = datetime.datetime.now()
+        )
+        # Try adding a second command to test
+        member2 = self.generateNewUserObject()
+        EventComment.objects.create(
+            user = member2,
+            event = eventObject,
+            content = "Hello World",
+            createdAt = datetime.datetime.now()
+        )
+        
+        response = c.get(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.json()['count'] == 2)
+
+    def test_show_event_comment_as_a_non_accepted_member_fails(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'comment/show/' + str(eventObject.id) + '/'
+
+        # Authenticated user is event owner
+        # Authenticate user-------------------------------------------
+        self.createNewUser()
+        user = User.objects.get(username=self.newUser['username'])
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        EventMember.objects.create(
+            eventId = eventObject.id,
+            event = eventObject,
+            userId = user.id,
+            user = user,
+            status = EventMemberStatus.get.PENDING.value
+        )
+
+        member1 = self.generateNewUserObject()
+        EventComment.objects.create(
+            user = member1,
+            event = eventObject,
+            content = "Hello World",
+            createdAt = datetime.datetime.now()
+        )
+        
+        response = c.get(url, {}, format='json')
+        self.assertFalse(response.status_code == status.HTTP_200_OK)
+
+    def test_show_event_comment_as_non_member_fails(self):
+        eventObject = self.generateNewEventObject()
+        url = self.baseUrl + 'comment/show/' + str(eventObject.id) + '/'
+
+        # Authenticated user is event owner
+        # Authenticate user-------------------------------------------
+        self.createNewUser()
+        user = User.objects.get(username=self.newUser['username'])
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+        
+        response = c.get(url, {}, format='json')
+        self.assertFalse(response.status_code == status.HTTP_200_OK)
+
+
+
+
+
+
+
+
