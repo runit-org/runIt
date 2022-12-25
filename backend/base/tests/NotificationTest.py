1c2a8062708e1e13e8485445817bfeab5ba5b5c7
juliantjg <juliantj88@gmail.com>
2022-12-25 16:24:48 +1100

    notification tests finished

--- backend/base/tests/NotificationTest.py
+++ backend/base/tests/NotificationTest.py
@@ -6,7 +6,7 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 from rest_framework.test import force_authenticate
-from base.enums import UserVoteStatus, EventStatus, EventMemberStatus
+from base.enums import UserVoteStatus, EventStatus, EventMemberStatus, NotificationStatus
 import random
 import string
 import datetime
@@ -14,7 +14,7 @@ from django.utils import timezone
 
 class NotificationTestClass(TestCase):
     newUser = None
-    baseUrl = '/api/notification/'
+    baseUrl = '/api/notifications/'
 
     def setUp(self):
         self.newUser = {
@@ -52,3 +52,65 @@ class NotificationTestClass(TestCase):
             email      = randomUserData['email'],
             password   = randomUserData['password'] 
         )
+
+    def generateNewNotificationObject(self, user):
+        return Notification.objects.create(
+            userId = user.id,
+            details = 'Hello World',
+            link = '',
+
+            createdAt = timezone.make_aware(datetime.datetime.now())
+        )
+    
+    def test_get_current_user_notifications_success(self):
+        url = self.baseUrl + 'all/'
+        user = self.generateNewUserObject()
+
+        self.generateNewNotificationObject(user)
+        self.generateNewNotificationObject(user)
+
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=user.username)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        response = c.get(url, {}, format='json')
+        print(response)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.json()['count'] == 2)
+
+    def test_read_one_user_notification_success(self):
+        user = self.generateNewUserObject()
+        notif = self.generateNewNotificationObject(user)
+
+        url = self.baseUrl + 'read/' + str(notif.id) + '/'
+
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=user.username)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        response = c.patch(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(Notification.objects.get(id=notif.id).status == NotificationStatus.get.READ.value)
+
+    def test_read_all_user_notification_success(self):
+        url = self.baseUrl + 'readAll/'
+        user = self.generateNewUserObject()
+
+        self.generateNewNotificationObject(user)
+        self.generateNewNotificationObject(user)
+        self.generateNewNotificationObject(user)
+
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=user.username)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        response = c.patch(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(Notification.objects.filter(userId=user.id, status=NotificationStatus.get.UNREAD.value)) == 0)
+    
