a3f2d3e9c09e9098d154900dbb08838e755fbca0
Manan Lodhia <mananlodhia@hotmail.com>
2023-01-20 10:48:26 +1100

    set up component test

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -9,13 +9,13 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     baseUrl: "http://localhost:3000",
-    specPattern: [
+    /*  specPattern: [
       "./cypress/e2e/signup.cy.js",
       "./cypress/e2e/login.cy.js",
       "./cypress/e2e/singleClick.cy.js",
       "./cypress/e2e/logout.cy.js",
       "./cypress/e2e/resetPassword.cy.js",
-    ],
+    ], */
   },
 
   component: {
