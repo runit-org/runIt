2de8205acddda6d685da5fdd80e90b3fd9208be1
Manan Lodhia <mananlodhia@hotmail.com>
2023-01-20 14:50:42 +1100

    updated logout test

--- frontend/cypress/component/logout_spec.cy.js
+++ frontend/cypress/component/logout_spec.cy.js
@@ -9,11 +9,12 @@ describe("Logout", () => {
     cy.fixture("user_creds").then((user) => {
       cy.intercept("POST", "/api/auth/login/", {
         statusCode: 200,
-      });
+      }).as("login");
       cy.findByRole("textbox").type(user.username);
       cy.findByLabelText(/password/i).type(user.password);
       cy.findByRole("button", { name: /login/i }).click();
-
+      cy.wait("@login");
+      cy.getCookie("token").should("exist");
       //logout
       cy.mount(<Route path={"/"} element={<Header />} />);
       cy.intercept("POST", "/api/auth/token/refresh/", {
@@ -21,10 +22,11 @@ describe("Logout", () => {
       });
       cy.intercept("POST", "/api/auth/logout/", {
         statusCode: 200,
-      });
+      }).as("logout");
       cy.findByRole("button", { name: /img/i }).click();
       cy.findByRole("button", { name: /logout/i }).click();
-      cy.getCookie("token").should("not.exist");
+      cy.wait("@logout");
+      cy.getCookie("token").should("to.be.null");
     });
   });
 });
