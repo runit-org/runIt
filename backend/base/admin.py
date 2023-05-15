97ddd8cee2b565ce258c644088b1b872f142ac2e
juliantjg <juliantj88@gmail.com>
2023-05-15 10:00:59 +1000

    added models on admin

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+admin.site.register(UserExtend)
 admin.site.register(Event)
+admin.site.register(EventMember)
+admin.site.register(EventCategory)
+admin.site.register(EventComment)
+admin.site.register(EventCommentLike)
 admin.site.register(Notification)
+admin.site.register(UserVote)
+admin.site.register(Friend)
+admin.site.register(FriendRequest)
