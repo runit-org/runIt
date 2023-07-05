25c52fe7a25571e131ee8c289d4825f7a90e1479
juliantjg <juliantj88@gmail.com>
2023-07-05 18:35:52 +1000

    removed comments

--- backend/base/apps.py
+++ backend/base/apps.py
@@ -1,10 +1,5 @@
 from django.apps import AppConfig
-# from .tasks import background_task
 
 class BaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'base'
-
-    # def ready(self):
-    #     # Start the background task when the app is ready
-    #     background_task()
