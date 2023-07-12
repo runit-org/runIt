2fc21366ece18f75b1b7881e8df48cddf748ae5f
juliantjg <juliantj88@gmail.com>
2023-07-12 10:45:15 +1000

    moved seeders, added UserSeeder

---
+++ backend/base/seeds/Seeders.py
@@ -0,0 +1,8 @@
+# Import all seeders here
+
+from base.seeds import UserSeeder
+from base.seeds import UserVoteSeeder
+from base.seeds import EventSeeder
+from base.seeds import EventMemberSeeder
+from base.seeds import EventCommentSeeder
+from base.seeds import EventCommentLikeSeeder
