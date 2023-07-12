df1bb1ccd257d158a5a5669713348f18a696a59d
juliantjg <juliantj88@gmail.com>
2023-07-12 18:29:44 +1000

    added factory (factory_boy)

---
+++ backend/base/factories/UserFactory.py
@@ -0,0 +1,10 @@
+import factory
+from base.models import User
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f'user{n}')
+    email = factory.Faker('email')
+    password = factory.Faker('password')
