81b5b784530813bb09ae52cde9bdc0c94588eb9f
juliantjg <juliantj88@gmail.com>
2023-07-13 18:15:25 +1000

    user factory update

--- backend/base/factories/UserFactory.py
+++ backend/base/factories/UserFactory.py
@@ -1,5 +1,6 @@
 import factory
 from base.models import User
+from django.contrib.auth.hashers import make_password
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -7,5 +8,4 @@ class UserFactory(factory.django.DjangoModelFactory):
 
     username = factory.Sequence(lambda n: f'user{n}')
     email = factory.Sequence(lambda n: f'user{n}@example.com')
-    password = factory.Faker('password')
-    
+    password = make_password('password')
