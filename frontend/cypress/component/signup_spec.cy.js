682d3fade989b0448a42b55c3b1bdee7146bfeae
Manan Lodhia <mananlodhia@hotmail.com>
2023-07-20 23:58:10 +1000

    refactored cypress test

--- frontend/cypress/component/signup_spec.cy.js
+++ frontend/cypress/component/signup_spec.cy.js
@@ -20,7 +20,6 @@ describe("Signup form", () => {
       cy.intercept("POST", "/api/auth/register/", {
         statusCode: 200,
       }).as("signup");
-      cy.findByRole("textbox", { name: /^name/i }).type(user.name);
       cy.findByRole("textbox", { name: /username/i }).type(user.username);
       cy.findByRole("textbox", { name: /email/i }).type(user.email);
       cy.findByLabelText(/^password/i).type(user.password);
