f13eaa669e317922b1c371699629d8b5f9fc7343
juliantjg <juliantj88@gmail.com>
2023-07-26 10:13:02 +1000

    minimum username length on registration

--- backend/base/modules/auth/api/validators/RegisterUserValidator.py
+++ backend/base/modules/auth/api/validators/RegisterUserValidator.py
@@ -6,6 +6,9 @@ def validate(request):
     data = request.data
     if data.get('email') == None or data.get('username') == None or data.get('password') == None or data.get('c_password') == None:
         return validationError('Required fields not met')
+    
+    if len(data['username']) < 5:
+        return validationError('Username is too short (5 chars min)')
 
     if len(data['username']) > 30:
         return validationError('Username is too long (30 chars max)')
@@ -18,11 +21,17 @@ def validate(request):
     
     password_regex = r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
     if not re.match(password_regex, data['password']):
-        return validationError('Invalid password format', {'password' : [
-            'Minimum 8 characters',
-            'At least 1 number',
-            'At least 1 alphabetical letter',
-            'At least 1 special character (e.g., @$!%*#?&)'
-        ]})
+        return validationError('Invalid password format', [
+                {
+                    'password' : 
+                        [
+                            'Minimum 8 characters',
+                            'At least 1 number',
+                            'At least 1 alphabetical letter',
+                            'At least 1 special character (e.g., @$!%*#?&)'
+                        ]
+                }
+            ]
+        )
 
     return None
