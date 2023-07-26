fef9880ae886d8b986f98b06e87ffb3672758af3
juliantjg <juliantj88@gmail.com>
2023-07-26 18:23:15 +1000

    change password API finished

---
+++ backend/base/modules/user/api/actions/ChangePasswordAction.py
@@ -0,0 +1,16 @@
+from base.models import User, UserExtend
+from base.views.baseViews import response, error
+from django.contrib.auth.hashers import make_password, check_password
+
+def update(request):
+    data = request.data
+    user = request.user
+
+    if not check_password(data['current_password'], user.password):
+        return error('Wrong current password')
+
+    user.password = make_password(data['password'])
+    user.save()
+
+    return response('Password changed successfully')
+    
