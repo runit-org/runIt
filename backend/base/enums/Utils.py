60d4b2824d65077cf57e709ba4f56956efd3e9e1
juliantjg <juliantj88@gmail.com>
2023-08-03 10:45:16 +1000

    updated EmailVerify model

--- backend/base/enums/Utils.py
+++ backend/base/enums/Utils.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
 class get(Enum):
-    DEFAULT_EMAIL_SENDER  = 'runIt'
-    TEST_USER_PASSWORD    = 'password123*'
+    DEFAULT_EMAIL_SENDER                 = 'runIt'
+    TEST_USER_PASSWORD                   = 'password123*'
+    EMAIL_VERIFICATION_TOKEN_EXPIRY_TIME = 180 #seconds
--- backend/base/modules/user/data/EmailVerify.py
+++ backend/base/modules/user/data/EmailVerify.py
@@ -1,10 +1,19 @@
 from django.db import models
 from base.models import *
+from datetime import datetime, timezone
+from base.enums import Utils
 
 class EmailVerify(models.Model):
-    email = models.CharField(max_length=200, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     token = models.CharField(max_length=200, null=True, blank=True)
-    created_at = models.DateTimeField(null=True)
+    createdAt = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.id
+    
+    def isTokenExpired(self):
+        timeNow = datetime.now(timezone.utc)
+        timeTokenWasCreated = self.createdAt
+        timeDifference = (timeNow - timeTokenWasCreated).seconds
+
+        return timeDifference > Utils.get.PASSWORD_RESET_EXPIRY_TIME.value
