31d1bb9eea444d7d5e721d1a233c5b54243b1081
juliantjg <juliantj88@gmail.com>
2023-08-04 10:43:36 +1000

    MIDDLEWARE: IsEmailVerified

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
 }
 
 from datetime import timedelta
