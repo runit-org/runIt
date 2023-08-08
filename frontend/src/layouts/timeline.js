02e79d70ef740195534a7f78ce11a09aa57508c4
Manan Lodhia <mananlodhia@hotmail.com>
2023-08-08 23:00:34 +1000

    timeline ui

--- frontend/src/layouts/timeline.js
+++ frontend/src/layouts/timeline.js
@@ -14,10 +14,12 @@ function Timeline() {
             <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
           </svg>
         </span>
-        <h3 className="d-flex align-items-center mb-1">
+        <h5 className="d-flex align-items-center mb-1">
           Application UI v2.0.0 <Badge>Latest</Badge>
-        </h3>
-        <time className="d-block mb-2">Released on January 13th, 2022</time>
+        </h5>
+        <time className="d-block mb-2 text-muted">
+          Released on January 13th, 2022
+        </time>
         <p>
           Get access to over 20+ pages including a dashboard layout, charts,
           kanban board, calendar, and pre-order E-commerce & Marketing pages.
