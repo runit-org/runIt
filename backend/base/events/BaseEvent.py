06ba38c00511b0dd7e6293349bd42a53bd3e263f
juliantjg <juliantj88@gmail.com>
2023-08-14 11:34:17 +1000

    base event fix

--- backend/base/events/BaseEvent.py
+++ backend/base/events/BaseEvent.py
@@ -1,13 +1,13 @@
 from events import Event
 
 class BaseEvent(Event):
-    _listeners = []
-
     def __init__(self):
         pass
 
     @classmethod
     def register(cls, listener):
+        if not hasattr(cls, '_listeners'):
+            cls._listeners = []  # Create a unique list for each event class
         cls._listeners.append(listener)
 
     @classmethod
