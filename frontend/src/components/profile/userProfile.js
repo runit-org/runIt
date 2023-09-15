c6f2ec1bf0996a90daa183bd63e2d27ae166f89e
Manan Lodhia <mananlodhia@hotmail.com>
2023-09-15 13:16:24 +1000

    refactored responseItems

--- frontend/src/components/profile/userProfile.js
+++ frontend/src/components/profile/userProfile.js
@@ -5,51 +5,58 @@ import Vote from "./vote";
 import { Username } from "../../layouts/username";
 import UserStatus from "./userStatus";
 import { useVerifyAuthUser } from "../../hooks/useCheckCurrUser";
+import { Loading } from "../../layouts/loader";
 
 function UserProfile() {
   const { authUser, user } = useVerifyAuthUser();
+  if (!user) {
+    return <Loading />;
+  }
+  const {
+    gravatarImage,
+    username,
+    email,
+    totalVote,
+    statusMessage,
+    last_login,
+    numParticipatedEvents,
+  } = user;
 
   return (
-    <>
-      {user ? (
-        <div className="w-100">
-          <div className="d-flex align-items-center userInfo-div">
-            <DisplayImage image={user.gravatarImage} />
-            <div className="ms-3">
-              <Username username={user.username} />
-              <small className="d-block text-muted">{user.email}</small>
-            </div>
+    <div className="w-100">
+      <div className="d-flex align-items-center userInfo-div">
+        <DisplayImage image={gravatarImage} />
+        <div className="ms-3">
+          <Username username={username} />
+          <small className="d-block text-muted">{email}</small>
+        </div>
 
-            <div className="position-absolute top-0 end-0 p-1">
-              {!authUser ? (
-                <Vote user={user} fullW={false} />
-              ) : (
-                <VoteBadge votes={user.totalVote} />
-              )}
-            </div>
-          </div>
-
-          {authUser ? (
-            <div className="my-3">
-              <UserStatus />
-            </div>
+        <div className="position-absolute top-0 end-0 p-1">
+          {!authUser ? (
+            <Vote user={user} fullW={false} />
           ) : (
-            <small className="d-block text-muted content_sm5">
-              {user.statusMessage}
-            </small>
+            <VoteBadge votes={totalVote} />
           )}
+        </div>
+      </div>
 
-          <div className="mt-3 ">
-            <UserCardInfo
-              lastLogin={user.last_login}
-              numParticipatedEvents={user.numParticipatedEvents}
-            />
-          </div>
+      {authUser ? (
+        <div className="my-3">
+          <UserStatus />
         </div>
       ) : (
-        ""
+        <small className="d-block text-muted content_sm5">
+          {statusMessage}
+        </small>
       )}
-    </>
+
+      <div className="mt-3 ">
+        <UserCardInfo
+          lastLogin={last_login}
+          numParticipatedEvents={numParticipatedEvents}
+        />
+      </div>
+    </div>
   );
 }
 
--- frontend/src/layouts/responseItems.js
+++ frontend/src/layouts/responseItems.js
@@ -50,35 +50,39 @@ export const ResponseToast = (props) => {
 
 export const ResponseItem = () => {
   const { response } = useContext(ResponseContext) || false;
+  const { OK, BAD_REQUEST, UNAUTHORIZED, UNPROCESSABLE_ENTITY } =
+    ResponseStatus;
 
-  return (
-    <>
-      {response ? (
-        response.status === ResponseStatus.OK ? (
-          <small className="text-success">{response.message}</small>
-        ) : response.status === ResponseStatus.BAD_REQUEST ||
-          response.status === ResponseStatus.UNAUTHORIZED ||
-          response.status === ResponseStatus.UNPROCESSABLE_ENTITY ? (
-          response.info.password ? (
-            <>
-              <small className="text-danger fw-bold">{response.message}</small>
-              {response.info.password.map((item, index) => {
-                return (
-                  <small className="text-danger" key={index}>
-                    {item}
-                  </small>
-                );
-              })}
-            </>
-          ) : (
-            <small className="text-danger">{response.message}</small>
-          )
-        ) : (
-          ""
-        )
-      ) : (
-        ""
-      )}
-    </>
-  );
+  // Helper function to render password error items
+  const renderErrorItems = (errors) => {
+    return errors.map((item, index) => (
+      <small className="text-danger" key={index}>
+        {item}
+      </small>
+    ));
+  };
+
+  // Render component based on response status
+  const renderResponse = () => {
+    if (!response) return null;
+
+    switch (response.status) {
+      case OK:
+        return <small className="text-success">{response.message}</small>;
+      case BAD_REQUEST:
+      case UNAUTHORIZED:
+      case UNPROCESSABLE_ENTITY:
+        return (
+          <>
+            <small className="text-danger fw-bold">{response.message}</small>
+            {Object.keys(response.info).length !== 0
+              ? renderErrorItems(response.info[0].password)
+              : null}
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+  return <>{renderResponse()}</>;
 };
