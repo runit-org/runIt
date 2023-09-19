cfbee4ff425fc617b010acd70cfec60174c34fb7
juliantjg <juliantj88@gmail.com>
2023-09-19 14:29:34 +1000

    content length for comments

--- backend/base/modules/eventComment/api/validators/CreateEventCommentValidator.py
+++ backend/base/modules/eventComment/api/validators/CreateEventCommentValidator.py
@@ -1,4 +1,5 @@
 from base.views.baseViews import validationError
+from base.enums import Utils
 
 def validate(request):
     data = request.data
@@ -8,5 +9,8 @@ def validate(request):
 
     if str(data['content']) == '' or str(data['content']).isspace() == True:
         return validationError('Content cannot be empty')
+    
+    if len(data['content']) > Utils.get.MAX_CONTENT_LENGTH.value:
+        return validationError('Content too long: ' + Utils.get.MAX_CONTENT_LENGTH.value)
 
     return None
