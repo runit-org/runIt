039ba3c78329461df52e5b30c6e846114cf2b638
juliantjg <juliantj88@gmail.com>
2023-09-19 14:30:57 +1000

    event comment details length

--- backend/base/modules/eventComment/data/EventComment.py
+++ backend/base/modules/eventComment/data/EventComment.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from base.models import *
+from base.enums import Utils
 
 class EventComment(models.Model):
     event = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    content = models.CharField(max_length=200, null=True, blank=True)
+    content = models.CharField(max_length=Utils.get.MAX_CONTENT_LENGTH.value, null=True, blank=True)
 
     createdAt = models.DateTimeField(null=True, blank=True)
 
