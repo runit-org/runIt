20a7bd4c5ec4785f088095afa6e4e414e1c062b0
juliantjg <juliantj88@gmail.com>
2023-09-19 15:01:27 +1000

    feedback length validator

--- backend/base/modules/feedback/api/validators/SendFeedbackValidator.py
+++ backend/base/modules/feedback/api/validators/SendFeedbackValidator.py
@@ -1,9 +1,13 @@
 from base.views.baseViews import validationError
+from base.enums import Utils
 
 def validate(request):
     data = request.data
 
     if data.get('details') == None or str(data['details']) == '':
         return validationError('Please provide the details field')
+    
+    if len(data['details']) > Utils.get.MAX_CONTENT_LENGTH.value:
+        return validationError('Details is too long')
 
     return None
