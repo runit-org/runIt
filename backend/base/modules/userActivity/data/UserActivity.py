6fbf4212370ac3150932faf8c95d839e18ccb30b
juliantjg <juliantj88@gmail.com>
2023-09-19 15:03:49 +1000

    additional length validators

--- backend/base/modules/userActivity/data/UserActivity.py
+++ backend/base/modules/userActivity/data/UserActivity.py
@@ -1,9 +1,10 @@
 from django.db import models
 from base.models import *
+from base.enums import Utils
 
 class UserActivity(models.Model):
     userId      = models.IntegerField(null=True, blank=True)
-    details     = models.CharField(max_length=500, null=True, blank=True)
+    details     = models.CharField(max_length=Utils.get.MAX_CONTENT_LENGTH.value*2, null=True, blank=True)
     link        = models.CharField(max_length=300, null=True, blank=True)
 
     createdAt   = models.DateTimeField(auto_now_add=True)
