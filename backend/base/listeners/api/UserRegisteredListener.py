1fbc985f53f841f0ecab76f9cb909905320bcf43
juliantjg <juliantj88@gmail.com>
2023-09-19 16:48:49 +1000

    log activity on user register

--- backend/base/listeners/api/UserRegisteredListener.py
+++ backend/base/listeners/api/UserRegisteredListener.py
@@ -1,11 +1,15 @@
 from events import EventListener
 from base.events.api import UserRegistered
-from base.traits import NotifyUser
+from base.traits import NotifyUser, LogUserActivity
 from base.modules.auth.api.actions import SendEmailOTPAction
 
 def sendNotification(user):
     message = 'Welcome to runIt, <b>' + user.username + '</b>! Please verify your email for full access.'
     NotifyUser.notify(user.id, message)
+
+def logUserActivity(user):
+    details = 'Joined runIt'
+    LogUserActivity.log(user.id, details)
 
 class UserRegisteredListener(EventListener):
     listensFor = [
@@ -15,6 +19,7 @@ class UserRegisteredListener(EventListener):
     def handle_event(self, event):
         user = event.user
         sendNotification(user)
+        logUserActivity(user)
         SendEmailOTPAction.send(user)
 
         pass
