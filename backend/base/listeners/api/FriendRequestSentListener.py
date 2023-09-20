63d94aad2b43cc808acbe594a4a37784576b4b71
juliantjg <juliantj88@gmail.com>
2023-09-20 12:40:52 +1000

    request friendship activity log

--- backend/base/listeners/api/FriendRequestSentListener.py
+++ backend/base/listeners/api/FriendRequestSentListener.py
@@ -1,12 +1,19 @@
 from events import EventListener
 from base.events.api import FriendRequestSent
-from base.traits import NotifyUser
+from base.traits import NotifyUser, LogUserActivity
 
 def sendNotification(target, user):
     link = '/friend-request/'
     notificationMessage = 'You have a pending friend request from <b>' + user.username + '</b>. Click to manage your friend requests.'  
     NotifyUser.notify(
         target.id, notificationMessage, link
+    )
+
+def logUserActivity(target, user):
+    link = '/profile?username=' + user.username
+    details = 'Requested friendship to user <b>' + target.username + '</b>.'  
+    LogUserActivity.log(
+        user.id, details, link
     )
 
 class FriendRequestSentListener(EventListener):
@@ -18,5 +25,6 @@ class FriendRequestSentListener(EventListener):
         target = event.target
         user = event.user
         sendNotification(target, user)
+        logUserActivity(target, user)
         pass
     pass
