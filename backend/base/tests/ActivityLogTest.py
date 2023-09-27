960b02e51e8ab3e18e346cd63b24d2521b2f725d
juliantjg <juliantj88@gmail.com>
2023-09-27 17:15:55 +1000

    update test on activity log based on new update

--- backend/base/tests/ActivityLogTest.py
+++ backend/base/tests/ActivityLogTest.py
@@ -7,7 +7,7 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIClient
 from rest_framework.test import force_authenticate
-from base.enums import UserVoteStatus, EventMemberStatus, Utils
+from base.enums import ActivityLogTypes
 import random
 import string
 from django.utils import timezone
@@ -23,11 +23,10 @@ class ActivityLogTestClass(BaseTestClass):
         self.newUser = UserFactory.build().__dict__
 
     def test_get_current_user_activity_log_success(self):
-        url = self.baseUrl
         user = self.generateNewUserObject()
-
         self.generateNewUserActivityObject(user)
 
+        url = self.baseUrl + user.username + '/'
         # Authenticate user-------------------------------------------
         user = User.objects.get(username=user.username)
         c = APIClient()
@@ -39,11 +38,10 @@ class ActivityLogTestClass(BaseTestClass):
         self.assertTrue(response.json()['count'] == 1)
 
     def test_activity_log_contains_title_and_type_success(self):
-        url = self.baseUrl
         user = self.generateNewUserObject()
-
         self.generateNewUserActivityObject(user)
 
+        url = self.baseUrl + user.username + '/'
         # Authenticate user-------------------------------------------
         user = User.objects.get(username=user.username)
         c = APIClient()
@@ -54,3 +52,37 @@ class ActivityLogTestClass(BaseTestClass):
         response = c.get(url, {}, format='json')
         # print(response.json()['results'][0])
         self.assertTrue(all(key in response.json()['results'][0] for key in keys))
+
+    def test_user_view_their_own_activity_logs_should_show_private_activities_success(self):
+        user = self.generateNewUserObject()
+        self.generateNewUserActivityObject(user, ActivityLogTypes.get.ACCOUNT.value)
+        self.generateNewUserActivityObject(user, ActivityLogTypes.get.FEEDBACK.value)
+
+        url = self.baseUrl + user.username + '/'
+        # Authenticate user-------------------------------------------
+        user = User.objects.get(username=user.username)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        response = c.get(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.json()['count'] == 2)
+
+    def test_user_view_others_activity_logs_should_not_show_private_activities_success(self):
+        user = self.generateNewUserObject()
+        self.generateNewUserActivityObject(user, ActivityLogTypes.get.ACCOUNT.value)
+        self.generateNewUserActivityObject(user, ActivityLogTypes.get.FEEDBACK.value)
+        self.generateNewUserActivityObject(user, ActivityLogTypes.get.FRIENDS.value)
+
+        url = self.baseUrl + user.username + '/'
+        # Authenticate user-------------------------------------------
+        differentUser = self.generateNewUserObject()
+        user = User.objects.get(username=differentUser.username)
+        c = APIClient()
+        c.force_authenticate(user=user)
+        # ------------------------------------------------------------
+
+        response = c.get(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.json()['count'] == 1)
