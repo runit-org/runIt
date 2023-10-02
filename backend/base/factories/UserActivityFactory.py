9f0861d359ea8b92f6f124c3c48730c5009efdef
juliantjg <juliantj88@gmail.com>
2023-10-02 17:33:14 +1100

    updated activity seed to include 2 years back till 2 weeks ago

--- backend/base/factories/UserActivityFactory.py
+++ backend/base/factories/UserActivityFactory.py
@@ -8,9 +8,9 @@ from base.enums import ActivityLogTypes
 
 def random_datetime():
     now = timezone.now()
-    min_datetime = now - timedelta(days=150)  # 5 months ago
-    max_datetime = now - timedelta(days=30)   # 1 month ago
-    random_days = random.randint(30, 150)     # Random number of days between 1 to 5 months
+    min_datetime = now - timedelta(days=720)  # 24 months ago
+    max_datetime = now - timedelta(days=14)   # 2 weeks ago
+    random_days = random.randint(14, 720)     # Random number of days between 2 weeks to 24 months
     random_delta = timedelta(days=random_days)
     return max_datetime - random_delta
 
