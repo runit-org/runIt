e0a5ea78a83768c966d55c6a95896e05cb0f876a
juliantjg <juliantj88@gmail.com>
2023-10-02 17:26:01 +1100

    added seed factory for months back

--- backend/base/factories/UserActivityFactory.py
+++ backend/base/factories/UserActivityFactory.py
@@ -1,8 +1,18 @@
 import factory
+import random
 from django.utils import timezone
+from datetime import timedelta
 from base.models import UserActivity
 from base.factories import UserFactory, EventFactory
 from base.enums import ActivityLogTypes
+
+def random_datetime():
+    now = timezone.now()
+    min_datetime = now - timedelta(days=150)  # 5 months ago
+    max_datetime = now - timedelta(days=30)   # 1 month ago
+    random_days = random.randint(30, 150)     # Random number of days between 1 to 5 months
+    random_delta = timedelta(days=random_days)
+    return max_datetime - random_delta
 
 class UserActivityFactory(factory.Factory):
     class Meta:
@@ -12,7 +22,7 @@ class UserActivityFactory(factory.Factory):
     link = factory.Faker('image_url')
     title = factory.Faker('sentence', nb_words=5)
     type = ActivityLogTypes.get.COMMENT.value
-    createdAt = factory.LazyFunction(timezone.now)
+    createdAt = factory.LazyFunction(random_datetime)
 
     @factory.lazy_attribute
     def userId(self):
