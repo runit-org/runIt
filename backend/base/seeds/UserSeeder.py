ac995120987a0a5ec401ae4b8ef69484366f51a8
juliantjg <juliantj88@gmail.com>
2023-10-04 18:15:45 +1100

    added more user and user votes seeders

--- backend/base/seeds/UserSeeder.py
+++ backend/base/seeds/UserSeeder.py
@@ -12,6 +12,17 @@ def create_seed_data():
     create_individual_user('evagreen', 'evagreen@email.com', 'password')
     create_individual_user('leexavier', 'leexavier@email.com', 'password')
     create_individual_user('walterhuberman', 'walterhuberman@email.com', 'password')
+    create_individual_user('johndavid', 'johndavid@email.com', 'password')
+    create_individual_user('michaelsmith', 'michaelsmith@email.com', 'password')
+    create_individual_user('sarahjones', 'sarahjones@email.com', 'password')
+    create_individual_user('emilybrown', 'emilybrown@email.com', 'password')
+    create_individual_user('davidwilson', 'davidwilson@email.com', 'password')
+    create_individual_user('laurasmith', 'laurasmith@email.com', 'password')
+    create_individual_user('matthewharris', 'matthewharris@email.com', 'passwords')
+    create_individual_user('olivertaylor', 'olivertaylor@email.com', 'password')
+    create_individual_user('williamclark', 'williamclark@email.com', 'password')
+    create_individual_user('hannahwright', 'hannahwright@email.com', 'password')
+
     print('Users seeded successfully')
 
 def create_individual_user(username, email, password):
--- backend/base/seeds/UserVoteSeeder.py
+++ backend/base/seeds/UserVoteSeeder.py
@@ -12,11 +12,20 @@ def create_seed_data():
     create_votes_for_one_user(8, UserVoteStatus.get.UPVOTE.value)
     create_votes_for_one_user(9, UserVoteStatus.get.UPVOTE.value)
     create_votes_for_one_user(10, UserVoteStatus.get.UPVOTE.value)
+    create_votes_for_one_user(11, UserVoteStatus.get.UPVOTE.value)
+    create_votes_for_one_user(12, UserVoteStatus.get.UPVOTE.value)
+    create_votes_for_one_user(13, UserVoteStatus.get.UPVOTE.value)
+    create_votes_for_one_user(14, UserVoteStatus.get.UPVOTE.value)
+    create_votes_for_one_user(15, UserVoteStatus.get.UPVOTE.value)
+    create_votes_for_one_user(16, UserVoteStatus.get.UPVOTE.value)
+    create_votes_for_one_user(17, UserVoteStatus.get.UPVOTE.value)
+    create_votes_for_one_user(18, UserVoteStatus.get.UPVOTE.value)
+    create_votes_for_one_user(19, UserVoteStatus.get.UPVOTE.value)
 
     print('User Votes seeded successfully')
 
 def create_votes_for_one_user(targetUserId, voteStatus):
-    for i in range(1, 6):
+    for i in range(1, 18):
         if i != targetUserId:
             create_individual_vote(i, targetUserId, voteStatus)
 
