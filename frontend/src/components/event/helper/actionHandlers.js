f1ffc80c8f005774d08fe83529b79ef7390c6a84
Manan Lodhia <mananlodhia@hotmail.com>
2023-10-09 15:04:41 +1100

    validation

--- frontend/src/components/event/helper/actionHandlers.js
+++ frontend/src/components/event/helper/actionHandlers.js
@@ -45,7 +45,7 @@ export const SingleEventHandler = (params, pageId) => {
 
   useEffect(() => {
     dispatch(getSingleEvent(params.id)).then((res) => {
-      let { status } = res.response;
+      let status = res?.response?.status || "";
       if (status === OK) {
         dispatch(getAllComments(params.id, pageId ? pageId : 1));
       } else if (status === BAD_REQUEST || status === SERVER_ERROR) {
