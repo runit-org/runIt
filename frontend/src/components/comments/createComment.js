64370f3c5744ea57a0e1c255705586efe86b3dfe
Manan Lodhia <mananlodhia@hotmail.com>
2023-10-17 11:51:38 +1100

    clean up

--- frontend/src/components/comments/createComment.js
+++ frontend/src/components/comments/createComment.js
@@ -102,6 +102,7 @@ function CreateComment(props) {
             <TaggingDropdown
               onCommentChange={handleCommentChange}
               formValue={formValue.content}
+              identifier="@"
             />
             <CTAButton
               type={"submit"}
--- frontend/src/components/comments/taggingDropdown.js
+++ frontend/src/components/comments/taggingDropdown.js
@@ -3,14 +3,14 @@ import { SingleEventContext } from "../../pages/singleEventDash";
 import { useContext, useEffect, useState } from "react";
 import { Accepted } from "../event/helper/eventBuilder";
 
-function TaggingDropdown({ onCommentChange, formValue }) {
+function TaggingDropdown({ onCommentChange, formValue, identifier }) {
   const { eventMbs } = useContext(SingleEventContext);
   const acceptedMembers = Accepted(eventMbs);
   const [showDropdown, setShowDropdown] = useState(false);
   const [dropdownArray, setDropdownArray] = useState([]);
 
   //extract the last tag
-  const lastword = (value) => {
+  const lastWord = (value) => {
     if (value) {
       const tags = value.split(" ");
       const lastTag = tags[tags.length - 1];
@@ -22,42 +22,43 @@ function TaggingDropdown({ onCommentChange, formValue }) {
 
   // show dropdown if input field has the tag identifier
   useEffect(() => {
-    if (lastword(formValue).includes("@")) {
+    if (lastWord(formValue).includes(identifier)) {
       setShowDropdown(true);
     } else {
       setShowDropdown(false);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [formValue]);
 
   const handleDropdownItemClick = (item) => {
     var newComment = item.username;
-    if (lastword(formValue).includes("@")) {
+    if (lastWord(formValue).includes(identifier)) {
       newComment = `${item.username} `;
     } else {
-      newComment = `@${item.username} `;
+      newComment = `${identifier}${item.username} `;
     }
 
     // Check if the content field is already populated
     // Check if the content field is already populated and the last word is a tag
     // If tag then replace the previous value with the tag value
     const updatedContent =
-      formValue && !lastword(formValue).includes("@")
+      formValue && !lastWord(formValue).includes(identifier)
         ? `${formValue}${newComment}`
-        : formValue && lastword(formValue).includes("@")
-        ? `${formValue.replace(lastword(formValue), "@")}${newComment}`
+        : formValue && lastWord(formValue).includes(identifier)
+        ? `${formValue.replace(lastWord(formValue), identifier)}${newComment}`
         : newComment;
 
     onCommentChange(updatedContent);
   };
 
   useEffect(() => {
-    // Regex to find words with @ symbol
-    const regex = /@(\w+)/g;
+    // Regex to find words with the identifier symbol
+    const regex = new RegExp(`${identifier}(\\w+)`, "g");
     const mentions = [];
     let match;
 
-    // Extract words with @ symbol from the text using the regex
-    while ((match = regex.exec(lastword(formValue))) !== null) {
+    // Extract words with identifier symbol from the text using the regex
+    while ((match = regex.exec(lastWord(formValue))) !== null) {
       let matchValue = match[1];
 
       // match the words with username in the object
@@ -79,7 +80,7 @@ function TaggingDropdown({ onCommentChange, formValue }) {
       onToggle={() => setShowDropdown(!showDropdown)}
     >
       <Dropdown.Toggle variant="light" size="sm" id="dropdown-tag">
-        @
+        {identifier}
       </Dropdown.Toggle>
       <Dropdown.Menu>
         {(dropdownArray.length > 0 ? dropdownArray : acceptedMembers).map(
