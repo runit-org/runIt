939c6f9c1d8705c40110436ed10013c7d43d1bdd
Manan Lodhia <mananlodhia@hotmail.com>
2023-12-11 14:56:46 +1100

    env check for redux store ext

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,10 +6,20 @@ const initialState = {};
 const middleware = [thunk];
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(
-  rootReducer,
-  initialState,
-  composeEnhancer(applyMiddleware(...middleware))
-);
+let store;
+
+if (process.env.NODE_ENV === "development") {
+  store = createStore(
+    rootReducer,
+    initialState,
+    composeEnhancer(applyMiddleware(...middleware))
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(...middleware)
+  );
+}
 
 export default store;
